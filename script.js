24a3762109e860ef3cfa909e089eb0f9578b0e49
Thiago de Freitas <ando.thiago@gmail.com>
2023-07-21 18:15:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,103 +5,123 @@ window.addEventListener("load", () => {
   const btnSection = document.querySelector(".botton");
   const btn2 = document.querySelector(".botton span:first-of-type");
   const formVal = document.querySelectorAll("form > input");
-  const pagNum = document.querySelectorAll("#header > div > span ");
+  const pagNum = document.querySelectorAll("#header  span ");
   const infoBox = document.querySelector(".print");
   const infoBoxMess = document.querySelector(".print > h3");
   const closeScreen = document.querySelector(".print > div");
+  const addPrice = document.querySelectorAll(".prc1");
+  const checkPlan = document.querySelectorAll(".bcolor");
   const block = document.querySelector("#container > div:first-of-type");
   const planBtn = document.getElementById("btn");
+  const finalPrice = document.querySelector(".prc3");
   const planBtnInside = document.querySelector(
-    "#planCont > div:last-of-type > #btn > div"
+    "#planCont > #holdBtn > #btn > div"
   );
   const monthly = document.querySelector(
-    "#planCont > div:last-of-type > div:first-of-type"
+    "#planCont > #holdBtn > div:first-of-type"
   );
   const yerly = document.querySelector(
-    "#planCont > div:last-of-type > div:last-of-type"
+    "#planCont > #holdBtn> div:last-of-type"
   );
   const price = document.querySelectorAll(".planType .txt p:nth-child(2)");
   const visible = document.querySelectorAll(".yerlyVisible");
-  const addPrice = document.querySelectorAll(".prc1");
-  const checkPlan = document.querySelectorAll(".bcolor");
+  const planType = document.querySelectorAll(".value");
+  const finalSum = document.querySelectorAll(".prc2");
+  const finalChange = document.querySelector("a");
   let checBox = document.querySelectorAll(".cheBox > input");
   let checBoxDiv = document.querySelectorAll(".boxJs");
+  let priceRangeYear = ["$90/yr", "$120/yr", "$150/yr"];
+  let priceRangeMonth = ["$9/mo", "$12/mo", "$15/mo"];
+  let priceRangeYearAdds = ["$10/yr", "$20/yr", "$20/yr"];
+  let priceRangeMonthAdds = ["$1/mo", "$2/mo", "$2/mo"];
   let choseCheck = [];
   let left = 0;
   let click = 0;
   let click2 = 0;
   let mORy = "mo";
   let pickPrice = null;
-  //   Information Box ================================
-  // printMessagge();
-  // function printMessagge(msg, num) {
-  //   msg =
-  //     msg ||
-  //     "This web page makes use of form validation. Make sure to Type your Name and Surname and use a valid email and a phone number similar to the one described in the form.";
+  let leftL = 500;
+  let steps = document.querySelectorAll(".onliDesk");
+  let x = window.matchMedia("(max-width: 441px)");
+  let doneResizing = null;
+  let planBcolor = "Arcade";
+  let last = "(Montly)";
+  //   Information Box ===============================================
+  printMessagge();
+  function printMessagge(msg, num) {
+    msg =
+      msg ||
+      "This web page makes use of form validation. Make sure to Type your Name and Surname and use a valid email and a phone number similar to the one described in the form.";
+    infoBox.classList.add("message");
+    block.classList.add("blockFrom");
+    infoBoxMess.innerHTML = msg;
+    closeScreen.onclick = () => {
+      infoBox.className = "print";
+      block.classList.remove("blockFrom");
+      if (num != undefined) {
+        formVal[num].focus();
+      }
+    };
+  }
 
-  //   infoBox.classList.add("message");
-  //   block.classList.add("blockFrom");
-  //   infoBoxMess.innerHTML = msg;
-  //   if (count == 2) count = 0;
-  //   closeScreen.onclick = () => {
-  //     infoBox.className = "print";
-  //     block.classList.remove("blockFrom");
-  //     formVal[num].focus();
-  //   };
-  // }
-
-  //   Information Box ====================e============
-
+  //   Information Box ==============================================
   //   Next step and Go back buttons ================================
-
   (function () {
     "user strict";
     btn.onclick = () => {
       if (click < formLen.length - 2 && formValidation()) {
         pageNumBackGround(click, 1);
         click++;
-        left += 350;
+        left += leftL;
         form.style.left = "-" + `${left}` + "px";
         if (click == formLen.length - 2) {
           btn.innerHTML = "Confirm";
           btn.style.backgroundColor = "hsl(243, 100%, 62%)";
           setFinalPrice();
         }
-        if (click == 1) {
+
+        if (click >= 2) {
+          backBtn();
+          btn2.style.color = "hsl(231, 11%, 63%)";
+          console.log(click);
+        }
+        if (click > 0 && click < 2) {
           planType[0].classList.add("bcolor");
           pickPrice = planType[0].getAttribute("value");
+          planBcolor = document.querySelector(".bcolor .txt p:first-of-type");
         }
-      } else if (click < formLen.length - 1) {
-        left += 350;
+      } else if (click > 0 && click < formLen.length - 1) {
+        left += leftL;
         form.style.left = "-" + `${left}` + "px";
         btnSection.className = "display";
       }
-      if (click >= 1) {
-        btn2.style.color = "hsl(231, 11%, 63%)";
-        backBtn();
+    };
+    function backBtn() {
+      btn2.addEventListener("click", backBtnPart2);
+    }
+    finalChange.onclick = (event) => {
+      event.preventDefault();
+      for (var i = 0; i < 2; i++) {
+        backBtnPart2();
       }
     };
-
-    function backBtn() {
-      btn2.onclick = () => {
-        if (click > 0) {
-          left = left - 350;
-          form.style.left = "-" + `${left}` + "px";
-          click--;
-          pageNumBackGround(click, 2);
-        }
-        if (click < 1) {
-          btn2.style.color = "hsl(0, 0%, 100%)";
-        }
-
-        if (click == 2) {
-          btn.innerHTML = "Next Step";
-          btn.style.backgroundColor = "hsl(213, 96%, 18%)";
-        }
-      };
+    function backBtnPart2() {
+      if (click >= 1) {
+        left = left - leftL;
+        form.style.left = "-" + `${left}` + "px";
+        click--;
+        pageNumBackGround(click, 2);
+      }
+      if (click < 2) {
+        btn2.style.color = "hsl(0, 0%, 100%)";
+        btn2.removeEventListener("click", backBtnPart2);
+      }
+      if (click == 2) {
+        btn.innerHTML = "Next Step";
+        btn.style.backgroundColor = "hsl(213, 96%, 18%)";
+      }
     }
   })();
-
   function pageNumBackGround(cllickNum, call) {
     switch (call) {
       case 2:
@@ -125,92 +145,91 @@ window.addEventListener("load", () => {
     }
   }
   //  End of  Next step and Go back buttons ================================
-
   //  Form Validation ======================================================
   function formValidation() {
-    //   var emp = [];
-    //   for (var i = 0; i < formVal.length; i++) {
-    //     if (formVal[i].value == "") {
-    //       emp.push(formVal[i].value);
-    //     }
-    //   }
-    //   empty();
-    //   function empty() {
-    //     if (
-    //       formVal[0].value == "" &&
-    //       formVal[1].value == "" &&
-    //       formVal[2].value == ""
-    //     ) {
-    //       printMessagge("Make sure to fill in the form");
-    //     } else {
-    //       checkEmpty();
-    //     }
-    //   }
-    //   function checkEmpty() {
-    //     for (var i = 0; i < formVal.length; i++) {
-    //       if (formVal[i].value == "") {
-    //         printMessagge("Make sure to fill in your " + formVal[i].name, i);
-    //         break;
-    //       }
-    //       if (count == 3) count = 2;
-    //     }
-    //   }
-    //   function checkName() {
-    //     value = formVal[0].value;
-    //     var condition = value.indexOf(" ") < 0;
-    //     if (!condition) {
-    //       return true;
-    //     } else {
-    //       printMessagge("Make sure to fill in Your Name and Surname", 0);
-    //     }
-    //   }
-    //   function checkEamil() {
-    //     value = formVal[1].value;
-    //     const pass = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-    //     if (pass) {
-    //       return true;
-    //     } else {
-    //       printMessagge("Make sure to enter a valid email", 1);
-    //     }
-    //   }
-    //   function checkNumber() {
-    //     valueExp = new RegExp(
-    //       "^\\+[3]{1}[5]{1}[3]{1}-[0-9]{2}-[0-9]{3}-[0-9]{4}$"
-    //     );
-    //     if (valueExp.test(formVal[2].value)) {
-    //       return true;
-    //     } else {
-    //       printMessagge("Make sure to enter a valid Phone Number", 2);
-    //     }
-    //   }
-    //   if (emp.length == 0) {
-    //     if (checkName() && checkEamil() && checkNumber()) {
-    //       return true;
-    //     }
-    //   }
-    return true;
+    if (click > 0) {
+      return true;
+    }
+    var emp = [];
+    for (var i = 0; i < formVal.length; i++) {
+      if (formVal[i].value == "") {
+        emp.push(formVal[i].value);
+      }
+    }
+    empty();
+    function empty() {
+      if (
+        formVal[0].value == "" &&
+        formVal[1].value == "" &&
+        formVal[2].value == ""
+      ) {
+        printMessagge("Make sure to fill in the form.");
+      } else {
+        checkEmpty();
+      }
+    }
+    function checkEmpty() {
+      for (var i = 0; i < formVal.length; i++) {
+        if (formVal[i].value == "") {
+          printMessagge(
+            "Make sure to fill in your " + formVal[i].name + ".",
+            i
+          );
+          break;
+        }
+      }
+    }
+    function checkName() {
+      value = formVal[0].value;
+      var condition = value.indexOf(" ") < 0;
+      if (!condition) {
+        return true;
+      } else {
+        printMessagge("Make sure to fill in Your Name and Surname.", 0);
+      }
+    }
+    function checkEamil() {
+      value = formVal[1].value;
+      const pass = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+      if (pass) {
+        return true;
+      } else {
+        printMessagge("Make sure to enter a valid email.", 1);
+      }
+    }
+    function checkNumber() {
+      valueExp = new RegExp(
+        "^\\+[3]{1}[5]{1}[3]{1}-[0-9]{2}-[0-9]{3}-[0-9]{4}$"
+      );
+      if (valueExp.test(formVal[2].value)) {
+        return true;
+      } else {
+        printMessagge("Make sure to enter a valid Phone Number.", 2);
+      }
+    }
+    if (emp.length == 0) {
+      if (checkName() && checkEamil() && checkNumber()) {
+        return true;
+      }
+    }
   }
   //  End of Form Validation ======================================================
-
-  //  Plans Type ==================================================================
-  let priceRangeYear = ["$90/yr", "$120/yr", "$150/yr"];
-  let priceRangeMonth = ["$9/mo", "$12/mo", "$15/mo"];
-  let priceRangeYearAdds = ["$10/yr", "$20/yr", "$20/yr"];
-  let priceRangeMonthAdds = ["$1/mo", "$2/mo", "$2/mo"];
-  const planType = document.querySelectorAll(".value");
-  const finalSum = document.querySelectorAll(".prc2");
-
+  //  Plans Type (month or Year)===================================================
   planBtn.onclick = () => {
     for (var i = 0; i < planType.length; i++) {
       planType[i].classList.remove("bcolor");
     }
-
     choseCheck = [];
-
     checBox.forEach((box) => {
       box.checked = false;
     });
-
+    checBoxDiv.forEach((box) => {
+      let atrr = box.getAttribute("value");
+      let atrrName = document.getElementById(atrr);
+      let disp = document.querySelector("." + atrrName.name);
+      disp.classList.add("display");
+      box.classList.remove("dcolor");
+    });
     if (click2 == 0) {
       planBtnInside.classList.add("transEfect");
       monthly.classList.add("color");
@@ -220,7 +239,6 @@ window.addEventListener("load", () => {
       });
       changePrice();
     }
-
     function changePrice() {
       let prcs = [];
       let prcsAdd = [];
@@ -231,12 +249,14 @@ window.addEventListener("load", () => {
           prcsAdd = priceRangeYearAdds;
           prcTimes = 10;
           mORy = "yr";
+          last = "(Yerly)";
           break;
         default:
           prcs = priceRangeMonth;
           prcsAdd = priceRangeMonthAdds;
           prcTimes = 0.1;
           mORy = "mo";
+          last = "(Montly)";
       }
       for (var i = 0; i <= 2; i++) {
         price[i].innerHTML = prcs[i];
@@ -252,7 +272,6 @@ window.addEventListener("load", () => {
         );
       }
     }
-
     if (click2 == 1) {
       planBtnInside.classList.remove("transEfect");
       monthly.classList.remove("color");
@@ -263,54 +282,85 @@ window.addEventListener("load", () => {
       changePrice();
     }
     click2++;
-
     if (click2 == 2) click2 = 0;
+    planType[0].classList.add("bcolor");
+    pickPrice = planType[0].getAttribute("value");
+    planBcolor = document.querySelector(".bcolor .txt p:first-of-type");
+    console.log();
   };
 
   (function () {
     "user strict";
     planType.forEach((plan) => {
       plan.addEventListener("click", function () {
-        planType[0].classList.add("bcolor");
-        pickPrice = planType[0].getAttribute("value");
         for (var i = 0; i < planType.length; i++) {
           planType[i].classList.remove("bcolor");
         }
         pickPrice = this.getAttribute("value");
         plan.classList.add("bcolor");
+        planBcolor = document.querySelector(".bcolor .txt p:first-of-type");
       });
     });
   })();
+  //  End of Plans Type (month or Year)=====================================
+  //  Chebox     ===========================================================
+  (function () {
+    "user strict";
+    checBoxDiv.forEach((box) => {
+      box.addEventListener("click", function () {
+        let atrr = this.getAttribute("value");
+        let atrrName = document.getElementById(atrr);
 
-  //  End of Plans Type ===========================================================
-
-  checBoxDiv.forEach((box) => {
-    box.addEventListener("click", function () {
-      let atrr = this.getAttribute("value");
-      let atrrName = document.getElementById(atrr);
-
-      if (atrrName.checked == false) {
-        document.getElementById(atrr).checked = true;
-        choseCheck.push(atrrName.value);
-        box.classList.add("dcolor");
-        let disp = document.querySelector("." + atrrName.name);
-        disp.classList.remove("display");
-      } else {
-        document.getElementById(atrr).checked = false;
-        box.classList.remove("dcolor");
-        choseCheck.splice(choseCheck.indexOf(atrrName.value), 1);
-      }
+        if (atrrName.checked == false) {
+          document.getElementById(atrr).checked = true;
+          choseCheck.push(atrrName.value);
+          box.classList.add("dcolor");
+          let disp = document.querySelector("." + atrrName.name);
+          disp.classList.remove("display");
+        } else {
+          document.getElementById(atrr).checked = false;
+          box.classList.remove("dcolor");
+          let disp = document.querySelector("." + atrrName.name);
+          disp.classList.add("display");
+          choseCheck.splice(choseCheck.indexOf(atrrName.value), 1);
+        }
+      });
     });
-  });
-
-  const finalPrice = document.querySelectorAll(".prc2");
-
+  })();
   function setFinalPrice() {
-    let sum = 0;
-    finalPrice[0].innerHTML = "&nbsp $" + `${pickPrice}` + "/" + mORy;
-    for (var i = 0; i < choseCheck.length; i++) {
-      sum += Number(choseCheck[i]);
-    }
-    choseCheck;
+    let finalPlan = document.querySelector("#arcade");
+    finalPlan.innerHTML = planBcolor.innerHTML + " " + last;
+    finalPrice.innerHTML = "&nbsp $" + `${pickPrice}` + "/" + mORy;
+    const total = choseCheck.reduce((currentTotal, item) => {
+      return Number(item) + currentTotal;
+    }, 0);
+    finalSum[3].innerHTML = "+$" + `${total + Number(pickPrice)}` + "/" + mORy;
   }
+  //  End of Chebox ======================================================
+  //======================= Mobile Version =================================
+  this.window.addEventListener("resize", function () {
+    this.clearTimeout(doneResizing);
+    doneResizing = this.setTimeout(function () {
+      responsive(x);
+    }, 100);
+  });
+  responsive(x);
+  function responsive(maxWidth) {
+    if (maxWidth.matches) {
+      displayTxt("none");
+      left = 0;
+      leftL = 350;
+    } else {
+      displayTxt("block");
+      left = 0;
+      leftL = 500;
+    }
+  }
+  function displayTxt(dis) {
+    steps.forEach((step) => {
+      step.style.display = dis;
+      form.style.left = "-" + 0 + "px";
+    });
+  }
+  this.clearTimeout(doneResizing);
 });
